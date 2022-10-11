31792a198ea01b72fc67522c24c7896bec68ddee
SEVADEC <111345755+SEVADEC@users.noreply.github.com>
2022-10-11 09:09:57 +0200

    Add files via upload

--- src/views/results/NotFound.js
+++ src/views/results/NotFound.js
@@ -1,24 +1,27 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-import Tile from 'components/misc/Tile'
-import Suggestions from 'components/misc/Suggestions'
+import Tile from "components/misc/Tile";
+import Suggestions from "components/misc/Suggestions";
 
 const Title = styled.h3`
   position: relative;
   margin-bottom: 1rem;
   font-size: 1.5em;
-`
+`;
 const Text = styled.p`
   position: relative;
   margin-bottom: 2em;
-`
+`;
 export default function NotFound(props) {
   return (
     <Tile>
-      <Title>Désolé, nous n'avons rien trouvé :(</Title>
-      <Text>Essayez un de ces produits :</Text>
+      <Title>Désolé, nous n'avons rien trouvé.</Title>
+      <Text>
+        Si un fruit ou légume n'est pas répertorié, c'est qu'il n'est pas
+        produit sur le Pays du Calaisis.
+      </Text>
       <Suggestions length={5} iframe={props.iframe} />
     </Tile>
-  )
+  );
 }
